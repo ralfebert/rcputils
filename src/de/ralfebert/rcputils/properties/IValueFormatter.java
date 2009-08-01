01abfed627acbf8a18c3cd374c15261a968fb8cd
Ralf Ebert <info@ralfebert.de>
2009-08-01 19:23:27 +0200

    docs

--- src/de/ralfebert/rcputils/properties/IValueFormatter.java
+++ src/de/ralfebert/rcputils/properties/IValueFormatter.java
@@ -2,6 +2,9 @@ package de.ralfebert.rcputils.properties;
 
 /**
  * An IValueFormatter describes a conversion for a value in both directions.
+ * Formatters are used for display and editing, that's why both directions are
+ * there. If you implement a formatter that's only for display, let the parse
+ * method throw an {@link UnsupportedOperationException}.
  * 
  * @author Ralf Ebert <info@ralfebert.de>
  */
