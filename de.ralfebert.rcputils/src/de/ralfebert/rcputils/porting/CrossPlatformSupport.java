6bf1d1fb919498c567d0fff058c33f102aa4b360
Ralf Ebert <info@ralfebert.de>
2010-02-27 14:19:29 +0100

    CrossPlatformSupport link to forum

--- de.ralfebert.rcputils/src/de/ralfebert/rcputils/porting/CrossPlatformSupport.java
+++ de.ralfebert.rcputils/src/de/ralfebert/rcputils/porting/CrossPlatformSupport.java
@@ -8,6 +8,8 @@ public class CrossPlatformSupport {
 	 * Determines if the given style bit is supported on the current Platform.
 	 * Information about this is to be added as needed. Returns always true if
 	 * not known.
+	 * 
+	 * See http://www.eclipse.org/forums/index.php?t=msg&th=163476&start=0&
 	 */
 	public static boolean isStyleSupported(int style) {
 		if ((style & (SWT.SEARCH | SWT.ICON_SEARCH | SWT.ICON_CANCEL)) > 0) {
