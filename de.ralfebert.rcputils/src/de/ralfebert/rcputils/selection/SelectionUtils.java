91db498521c8f9a52b86ad3937d1ff21bd3c70b7
Ralf Ebert <info@ralfebert.de>
2010-03-03 16:28:39 +0100

    SelectionUtils.getIterable returns empty list when selection is null

--- de.ralfebert.rcputils/src/de/ralfebert/rcputils/selection/SelectionUtils.java
+++ de.ralfebert.rcputils/src/de/ralfebert/rcputils/selection/SelectionUtils.java
@@ -17,15 +17,13 @@ public class SelectionUtils {
 	 * Returns a list of all objects of selectedObjectClass contained in the
 	 * given selection.
 	 */
-	public static final <A> Iterable<A> getIterable(ISelection selection,
-			Class<A> selectedObjectClass) {
-		if (selection.isEmpty())
+	public static final <A> Iterable<A> getIterable(ISelection selection, Class<A> selectedObjectClass) {
+		if (selection == null || selection.isEmpty())
 			return Collections.emptyList();
 
 		List<A> selectedObjects = new ArrayList<A>();
 		if (selection instanceof IStructuredSelection) {
-			Iterator<?> iterator = ((IStructuredSelection) selection)
-					.iterator();
+			Iterator<?> iterator = ((IStructuredSelection) selection).iterator();
 			while (iterator.hasNext()) {
 				Object obj = iterator.next();
 				if (selectedObjectClass.isAssignableFrom(obj.getClass())) {
