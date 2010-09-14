8f28852e5d30f196af40d51abbbf99fa799362da
Ralf Ebert <ralf@ralfebert.de>
2010-09-14 12:45:39 +0200

    SelectionUtils based on google collections

--- de.ralfebert.rcputils/src/org/eclipselabs/rcputils/selection/SelectionUtils.java
+++ de.ralfebert.rcputils/src/org/eclipselabs/rcputils/selection/SelectionUtils.java
@@ -1,12 +1,12 @@
 package org.eclipselabs.rcputils.selection;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+
+import com.google.common.collect.Iterators;
 
 /**
  * Helper methods for working with JFace selections.
@@ -15,26 +15,23 @@ public class SelectionUtils {
 
 	/**
 	 * Returns a list of all objects of selectedObjectClass contained in the
-	 * given selection.
+	 * given selection, for example:
+	 * 
+	 * <code>
+	 * for(Car car : SelectionUtils.getIterable(carViewer, Car.class)) {
+	 * 	// ...
+	 * }
+	 * </code>
 	 */
-	public static final <A> Iterable<A> getIterable(ISelection selection, Class<A> selectedObjectClass) {
-		if (selection == null || selection.isEmpty())
+	public static final <A> Iterable<A> getIterable(final ISelection selection, final Class<A> selectedObjectClass) {
+		if (selection == null || selection.isEmpty() || !(selection instanceof IStructuredSelection))
 			return Collections.emptyList();
 
-		List<A> selectedObjects = new ArrayList<A>();
-		if (selection instanceof IStructuredSelection) {
-			Iterator<?> iterator = ((IStructuredSelection) selection).iterator();
-			while (iterator.hasNext()) {
-				Object obj = iterator.next();
-				if (selectedObjectClass.isAssignableFrom(obj.getClass())) {
-					@SuppressWarnings("unchecked")
-					A selectedObject = (A) obj;
-					selectedObjects.add(selectedObject);
-				}
-
+		return new Iterable<A>() {
+			public Iterator<A> iterator() {
+				Iterator<?> iterator = ((IStructuredSelection) selection).iterator();
+				return Iterators.filter(iterator, selectedObjectClass);
 			}
-		}
-
-		return selectedObjects;
+		};
 	}
 }
